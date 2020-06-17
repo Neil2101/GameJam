932d82d56f0bd074ce0d23c1586f4d8fdc92a9c7
NeilYe <nanmuhongy4341@aurorak12.org>
2020-06-17 23:21:04 +0000

    Website AGAIN!

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import webbrowser
 """
 delete me after you fork this
 
@@ -8,8 +7,11 @@ can we make this game jam hit at least 10 participants?
 
 
 The youtube link is avalible 
-"""
+run this code on idle and you'll find the video
+
+import webbrowser
+
 webbrowser.open('https://www.youtube.com/watch?v=0R9NsLVyA6Y')
-"""
+
 topic (sorry Wowie Jam 2.0) Intentional Bugs
 """
